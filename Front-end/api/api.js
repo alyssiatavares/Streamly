06d309693f7dd6fbe272f3514032d9e81e8abf39
Alyssia Tavares <alyssiatavares@gmail.com>
2025-08-23 23:25:07 -0700

    Deploy changes

--- Front-end/api/api.js
+++ Front-end/api/api.js
@@ -1,8 +1,6 @@
-import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://streamly-v5o7.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artistspj`);
 const responseSongs = await axios.get(`${URL}/songs`);
