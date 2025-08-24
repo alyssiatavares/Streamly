3a4e3cad23d5d69bc70d8d81fc03b50a718869e4
Alyssia Tavares <alyssiatavares@gmail.com>
2025-08-23 23:13:22 -0700

    Deploy changes

--- Back-end/api/server.js
+++ Back-end/api/server.js
@@ -2,22 +2,30 @@ import "dotenv/config";
 import express from "express";
 import { initializeMongoClient } from "./connect.js";
 import cors from "cors"; // middleware to handle CORS
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
 const db = initializeMongoClient();
 app.use(cors());
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Hello, World!Updated / test/test2");
 });
 
-app.get("/artistspj", async (request, response) => {
+app.get("/api/artistspj", async (request, response) => {
   response.send(await db.collection("artistspj").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../Front-end/dist")));
+app.get("/{*any}", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../Front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
